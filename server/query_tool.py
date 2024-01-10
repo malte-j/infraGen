4ff9e37a719f40f80168b081e9f171e5a16c529e
Malte <hi@malts.me>
2024-01-10 02:11:47 +0100

    redis history working

--- server/query_tool.py
+++ server/query_tool.py
@@ -1,16 +1,17 @@
 from langchain.tools import tool
 from langchain.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+from sqlalchemy import desc
 
 db = Chroma(embedding_function=OpenAIEmbeddings(), persist_directory="./chroma_db")
 
 
 @tool
 def reference_code_search(query: str) -> str:
-    """Search for refernce code for specific architecture."""
+    """Search for terraform best practice reference code."""
     docs = db.similarity_search(query)
 
-    if len(docs) > 0:
-        return docs[0].page_content
+    # if len(docs) > 0:
+    #     return docs[0].page_content
 
     return "No results found"
